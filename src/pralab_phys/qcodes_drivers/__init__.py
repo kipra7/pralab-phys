ebe710198270bc666c805d76cfe4d64e7fa3844d
MKPra <124507187+kipra7@users.noreply.github.com>
2025-05-15 15:37:53 +0900

    0.3.1

--- src/pralab_phys/qcodes_drivers/__init__.py
+++ src/pralab_phys/qcodes_drivers/__init__.py
@@ -2,6 +2,6 @@
 
 from .yokogawa7651 import Yokogawa7651
 from .keithley2182a1ch import Keithley2182A1ch
-#from .QDdotNET import PPMSdotNET
-from .qdvisa import PPMSClassic
+from .qddotnet import QDdotNET
 from .keithley6221 import Keithley6221
+from .nfli5640 import nfLI5640
