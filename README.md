33bf78ec90400d2fe90e6755d08533f4c72f40e6
MKPra <124507187+kipra7@users.noreply.github.com>
2025-12-01 14:10:23 +0900

    update 3.11

--- README.md
+++ README.md
@@ -1,12 +1,63 @@
-# pralab-phys 0.2.1a1
+# pralab-phys 0.4.0
 
 Pralab-phys provides some extensions of QCoDes (python-based data acquisition framework) and Plotly and more.
 
 Especially, it includes following QCoDes Instrument Drivers
 
-- Keithley2182A (1ch only)
+- Keithley2182A
 - Keithley6221
-- Quantum Design PPMS (using GPIB connection)
+- Yokogawa7651
+- Quantum Design Instruments (PPMS, Dynacool)
 
 ### Attention: 
-For versions below 0.2.0, we **strongly discourage usage** due to instability in versioning and other concerns.
+For versions below 0.3.11, we **strongly discourage usage** due to instability in versioning and other concerns.
+
+
+# pralab-phys 0.4.0 日本語解説
+
+## 概要
+本ライブラリは、物性研究室で用いることを想定した、QCoDesをベースとした測定用ライブラリであり、これを用いることで、様々な計測器制御や測定プログラムの作成が容易に行えます。
+
+このライブラリは、QCoDesを中心としたライブラリの拡張ドライバとして、以下の機能を提供します。
+
+QCoDesの基本的な使い方については、[公式ドキュメント](https://microsoft.github.io/Qcodes/)またはqiita記事をご覧ください。
+
+### 各計測器の制御用ライブラリ
+- Keithley2182A Nanovoltmeter
+- keithley6221 DC and AC Current Source
+- Yokogawa7651 電流源
+- エヌエフ LI5650 ロックインアンプ
+- Quantum Design Multivu経由の装置制御（QDInstrument.dll必須）
+- M81 (予定)
+
+### より平易なリアルタイムプロット機能
+- RealtimePlotモジュールによる、Jupyter上での測定結果確認
+
+
+## インストール&最初の利用
+本ライブラリは、uvを用いたPython仮想環境上へのインストールを推奨しています。
+
+Windows Powershellを用いたインストール手順は以下の通りです。
+
+1. 仮想環境を作成したいディレクトリ上で以下のコードを入力（任意の仮想環境名を入力しない場合、該当ディレクトリにそのまま仮想環境が構築されます）
+
+```
+uv init <任意の仮想環境名>
+```
+
+2. pralab-physライブラリのインストール
+
+```
+uv add pralab-phys
+```
+
+3. jupyter labを起動する。自動的にブラウザが開き、Jupyter Labが使える状態になる。
+
+```
+uv run jupyter lab
+```
+
+もしブラウザが開かない場合、Powershell上に出力されたリンク(localhost:9999など)をブラウザに入力する。
+
+Jupyter Labを終了する場合、Powershell上でctrl+cを入力。
+
--- src/pralab_phys/qcodes_drivers/qddotnet.py
+++ src/pralab_phys/qcodes_drivers/qddotnet.py
@@ -10,6 +10,66 @@ from System import Double, UInt16, Int32
 DEFAULT_PORT = 11000
 
 class QDdotNET(Instrument):
+    """
+    A driver class for Quantum Design instruments, providing control over temperature, magnetic field, and position.
+
+    Parameters
+    ----------
+    name : str
+        The name of the instrument instance.
+    ip_address : str
+        The IP address of the Quantum Design instrument.
+    instruent_type : str, optional
+        The type of the instrument (default is "DynaCool").
+    remote : bool, optional
+        Whether to use remote control (default is False).
+    QDInstrumentdir : str, optional
+        Directory path to the Quantum Design instrument DLL (default is "").
+    QDInstrumentname : str, optional
+        Name of the Quantum Design instrument DLL (default is "QDInstrument").
+    port : int, optional
+        The port number for communication (default is 11000).
+    **kwargs : dict
+        Additional keyword arguments passed to the base class.
+
+    Attributes
+    ----------
+    temperature : QDTemperature
+        Parameter for controlling and reading the temperature.
+    temperaturestatus : QDTemperatureStatus
+        Parameter for reading the temperature status.
+    field : QDField
+        Parameter for controlling and reading the magnetic field.
+    fieldstatus : QDFieldStatus
+        Parameter for reading the magnetic field status.
+    position : QDPosition
+        Parameter for controlling and reading the position.
+    positionstatus : QDPositionStatus
+        Parameter for reading the position status.
+    fieldrate : Parameter
+        Parameter for setting and getting the magnetic field rate.
+    temperaturerate : Parameter
+        Parameter for setting and getting the temperature rate.
+    positionrate : Parameter
+        Parameter for setting and getting the position rate.
+
+    Methods
+    -------
+    get_field()
+        Retrieves the current magnetic field value.
+    set_field(field)
+        Sets the magnetic field to the specified value.
+    get_position()
+        Retrieves the current position of the motor.
+    set_position(position)
+        Sets the motor position to the specified value.
+    set_temperature(temp)
+        Sets the temperature to the specified value.
+    get_temperature()
+        Retrieves the current temperature value.
+    get_idn()
+        Returns the instrument identification information.
+    """
 
     def __init__(
         self,
@@ -152,6 +212,25 @@ class QDdotNET(Instrument):
 
 class QDPosition(Parameter):
     """
+    Parameter class for controlling and reading the motor position.
+
+    Parameters
+    ----------
+    name : str
+        The name of the parameter.
+    instrument : QDdotNET
+        The instrument instance this parameter belongs to.
+    **kwargs : dict
+        Additional keyword arguments.
+
+    Methods
+    -------
+    set_raw(position: float)
+        Sets the motor position to the specified value.
+    get_raw() -> float
+        Retrieves the current motor position.
+    """
+    """
     Parameter class for the motor position
     """
     def __init__(
@@ -173,6 +252,23 @@ class QDPosition(Parameter):
 
 class QDPositionStatus(Parameter):
     """
+    Parameter class for reading the motor position status.
+
+    Parameters
+    ----------
+    name : str
+        The name of the parameter.
+    instrument : QDdotNET
+        The instrument instance this parameter belongs to.
+    **kwargs : dict
+        Additional keyword arguments.
+
+    Methods
+    -------
+    get_raw() -> int
+        Retrieves the current motor position status.
+    """
+    """
     Parameter class for the motor position status
     """
     def __init__(
@@ -189,6 +285,25 @@ class QDPositionStatus(Parameter):
 
 
 class QDTemperature(Parameter):
+    """
+    Parameter class for controlling and reading the temperature.
+
+    Parameters
+    ----------
+    name : str
+        The name of the parameter.
+    instrument : QDdotNET
+        The instrument instance this parameter belongs to.
+    **kwargs : dict
+        Additional keyword arguments.
+
+    Methods
+    -------
+    set_raw(temperature: float)
+        Sets the temperature to the specified value.
+    get_raw() -> float
+        Retrieves the current temperature value.
+    """
     """
     Parameter class for the temperature
     """
@@ -211,6 +326,23 @@ class QDTemperature(Parameter):
 
 class QDTemperatureStatus(Parameter):
     """
+    Parameter class for reading the temperature status.
+
+    Parameters
+    ----------
+    name : str
+        The name of the parameter.
+    instrument : QDdotNET
+        The instrument instance this parameter belongs to.
+    **kwargs : dict
+        Additional keyword arguments.
+
+    Methods
+    -------
+    get_raw() -> int
+        Retrieves the current temperature status.
+    """
+    """
     Parameter class for the temperature
     """
     def __init__(
@@ -227,6 +359,25 @@ class QDTemperatureStatus(Parameter):
 
 
 class QDField(Parameter):
+    """
+    Parameter class for controlling and reading the magnetic field.
+
+    Parameters
+    ----------
+    name : str
+        The name of the parameter.
+    instrument : QDdotNET
+        The instrument instance this parameter belongs to.
+    **kwargs : dict
+        Additional keyword arguments.
+
+    Methods
+    -------
+    set_raw(mag: float)
+        Sets the magnetic field to the specified value.
+    get_raw() -> float
+        Retrieves the current magnetic field value.
+    """
     """
     Parameter class for the field
     """
